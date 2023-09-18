99c477945f6f319e25d53ccb6fbe4e8125a79092
Sauce2002 <jjpro2002@gmail.com>
2023-09-18 17:05:31 -0500

    si

--- problemas.cpp
+++ problemas.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <array>
 using namespace std;
 
 void problema2(){
@@ -15,11 +16,10 @@ void problema2(){
     char cadenaLetras[10] ={};
     char *punteroLetras = cadenaLetras;
 
-    for(int i=0;i<10;i++){
+    for(int i=0;i<sizeof(cadenaLetras);i++){
         int num2 =65+ rand() %(91-65);
         *(punteroLetras+i) = char(num2) ;
         cout << num2<<" ";
-
     }
 
     cout<<endl;
