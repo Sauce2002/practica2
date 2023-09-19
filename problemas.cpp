da04d1131a79f189bd6659d9eb83b2f05d17d67b
Sauce2002 <jjpro2002@gmail.com>
2023-09-19 16:13:53 -0500

    problema 2 listo

--- problemas.cpp
+++ problemas.cpp
@@ -9,9 +9,10 @@ void problema2(){
 
     srand(time(NULL));
 
-    char cadenaLetras[11] ={};
-    char abecedario[26]={A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+    char cadenaLetras[201] ={};
+    char abc[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     char *punteroLetras = cadenaLetras;
+    char *pAbc=abc;
 
     for(int i=0;i<sizeof(cadenaLetras);i++){
         int num2 =65+ rand() %(91-65);
@@ -22,6 +23,14 @@ void problema2(){
     cout<<endl;
     cout << cadenaLetras <<endl;
 
+    for(int i=0;i < sizeof(abc)-1;i++){
+        int contador=0;
+        for(int j=0;j<sizeof(cadenaLetras)-1;j++){
+            if(*(punteroLetras+j)==*(pAbc+i))contador+=1;
+        }
+        if(contador>0)cout<<*(pAbc+i)<<": "<<contador<<endl;
+
+    }
 
 }
 
