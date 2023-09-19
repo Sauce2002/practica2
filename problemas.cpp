83c2b7b37dbf9e6e2b833f89435eb3922c4e8b35
Sauce2002 <jjpro2002@gmail.com>
2023-09-19 15:49:15 -0500

    a

--- problemas.cpp
+++ problemas.cpp
@@ -8,15 +8,17 @@ using namespace std;
 void problema2(){
 
     srand(time(NULL));
-    char cadenaLetras[10] ={};
+
+    char cadenaLetras[11] ={};
+    char abecedario[26]={A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
     char *punteroLetras = cadenaLetras;
 
     for(int i=0;i<sizeof(cadenaLetras);i++){
         int num2 =65+ rand() %(91-65);
         *(punteroLetras+i) = char(num2) ;
-        cout << num2<<" ";
-    }
 
+    }
+    cadenaLetras[sizeof(cadenaLetras)-1]='\0';
     cout<<endl;
     cout << cadenaLetras <<endl;
 
